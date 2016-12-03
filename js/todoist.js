6bf25812b41b90a2519bae1a7d916b363dd8de80
Billy Wong <bwong5@stuy.edu>
2016-12-03 16:18:54 -0500

    more todoist process

--- js/todoist.js
+++ js/todoist.js
@@ -7,7 +7,7 @@ function main() {
 	
 	query = {
 		client_id: "08036ba1ba6f47e4ac6a04882ae53787",
-		state: "3be5d79578a94267aaaa3c11ade9d6b7",
+		state: "secret",
 		scope: ["data:read_write", "task:add"],
 		redirect_uri: "urn:ietf:wg:oauth:2.0:oob",
 	}
@@ -20,4 +20,20 @@ function main() {
 	window.location.href = url;
 	
 
+}
+
+function getToken(rCode) {
+	
+	query = {
+		client_id: "08036ba1ba6f47e4ac6a04882ae53787",
+		client_secret: "3be5d79578a94267aaaa3c11ade9d6b7",
+		code: rCode,
+	}
+	
+	var root = "https://todoist.com/oauth/access_token?"
+	var url = root + "client_id=" + query.client_id + "&client_secret=" + query.client_secret + "&code=" + query.code
+
+	window.location.href = url;
+	
+	
 }
